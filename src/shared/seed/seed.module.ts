5aa9ae5d831a97809af86a9f48326f3529ccfcc6
minh chiu <himinhchiu@gmail.com>
2023-05-31 00:59:50 +0700

    refactor: update directory number

--- src/shared/seed/seed.module.ts
+++ src/shared/seed/seed.module.ts
@@ -1,7 +1,7 @@
 import { EndpointModule } from '~pre-built/2-endpoints/endpoint.module';
-import { ProvinceModule } from '~pre-built/7-provinces/province.module';
-import { DistrictModule } from '~pre-built/8-districts/district.module';
-import { WardModule } from '~pre-built/9-wards/ward.module';
+import { ProvinceModule } from '~pre-built/8-provinces/province.module';
+import { DistrictModule } from '~pre-built/9-districts/district.module';
+import { WardModule } from '~pre-built/10-wards/ward.module';
 
 import { Module } from '@nestjs/common';
 
