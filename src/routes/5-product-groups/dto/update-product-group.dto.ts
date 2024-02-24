fdb1b892b564f26a7cbe93fb81e67b1f86fc6d0a
minh <minhchiu.official@gmail.com>
2024-02-25 01:12:20 +0700

    update: new package

--- src/routes/5-product-groups/dto/update-product-group.dto.ts
+++ src/routes/5-product-groups/dto/update-product-group.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from "@nestjs/mapped-types";
+import { PartialType } from "@nestjs/swagger";
 
 import { CreateProductGroupDto } from "./create-product-group.dto";
 
--- src/routes/pre-built/7-user-files/dto/update-user-file.dto.ts
+++ src/routes/pre-built/7-user-files/dto/update-user-file.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from "@nestjs/mapped-types";
+import { PartialType } from "@nestjs/swagger";
 
 import { CreateUserFileDto } from "./create-user-file.dto";
 
