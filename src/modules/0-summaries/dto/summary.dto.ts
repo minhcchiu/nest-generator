9bec64ae6d28874bf6ed0ffd01d0642ca554bef5
minh <minhchiu.official@gmail.com>
2024-11-01 01:28:32 +0700

    fix: validate date

--- src/modules/0-summaries/dto/summary.dto.ts
+++ src/modules/0-summaries/dto/summary.dto.ts
@@ -1,18 +1,17 @@
 import { Transform } from "class-transformer";
-import { IsISO8601, IsOptional } from "class-validator";
-import { parse } from "date-fns";
+import { IsDate, IsOptional } from "class-validator";
 import { ObjectId } from "mongodb";
 import { IsObjectId } from "~common/validators/objectId";
 
 export class SummaryDto {
   @IsOptional()
-  @IsISO8601()
-  @Transform(({ value }) => parse(value, "yyyy-MM-dd", new Date()))
+  @IsDate()
+  @Transform(({ value }) => new Date(value))
   from?: Date;
 
   @IsOptional()
-  @IsISO8601()
-  @Transform(({ value }) => parse(value, "yyyy-MM-dd", new Date()))
+  @IsDate()
+  @Transform(({ value }) => new Date(value))
   to?: Date;
 
   @IsOptional()
