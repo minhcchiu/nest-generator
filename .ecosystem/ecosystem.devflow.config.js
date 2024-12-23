74a9e3226f8eb01c5231001b5b6b43765f63e904
minh <minhchiu.official@gmail.com>
2024-12-23 10:28:30 +0700

    build: setup deploy devflow server

--- .ecosystem/ecosystem.devflow.config.js
+++ .ecosystem/ecosystem.devflow.config.js
@@ -1,16 +1,16 @@
 module.exports = {
   apps: [
     {
-      name: "asnest-dev",
+      name: "devflow-dev",
       script: "dist/main.js",
       instances: 1,
       autorestart: true,
       watch: false,
       time: true,
       max_memory_restart: "1G",
-      cwd: "/home/ubuntu/asnest-dev/source",
-      error_file: "/home/ubuntu/asnest-dev/logs/web.err.log",
-      out_file: "/home/ubuntu/asnest-dev/logs/web.out.log",
+      cwd: "/home/ubuntu/devflow-dev/source",
+      error_file: "/home/ubuntu/devflow-dev/logs/web.err.log",
+      out_file: "/home/ubuntu/devflow-dev/logs/web.out.log",
       exec_mode: "cluster",
       env: {
         NODE_ENV: "development",
